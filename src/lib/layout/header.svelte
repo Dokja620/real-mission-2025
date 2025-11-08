e5fd30191736d084691e15c91c3731fe840474d2
Dokja620 <revanamike620@gmail.com>
2025-11-08 17:11:06 +0300

    feat(Header): looks great

---
+++ src/lib/layout/header.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { setLocale } from "$lib/paraglide/runtime";
+  import Icon from "@iconify/svelte";
+  import logo from "$lib/assets/media/logo.webp";
+</script>
+
+<header>
+  <nav>
+    <a class="logo" href="/" aria-label="Go home link">
+      <img src={logo} alt="Real Mission mouvement logo" />
+    </a>
+
+    <ul class="nav-links">
+      <li>
+        <a href="/">Gallerie</a>
+      </li>
+    </ul>
+
+    <div class="lang">
+      <button onclick={() => setLocale("fr")}>
+        <Icon icon="twemoji:flag-france" width="1.75rem" />
+      </button>
+      <button onclick={() => setLocale("en")}>
+        <Icon icon="twemoji:flag-us-outlying-islands" width="1.75rem" />
+      </button>
+    </div>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import "../app.css";
   import "../scss/global.scss";
-  import favicon from "$lib/assets/favicon.svg";
+  import favicon from "$lib/assets/logo.webp";
 
   // Fonts
   import "@fontsource/poppins/400.css";
   import "@fontsource/poppins/500.css";
   import "@fontsource/poppins/700.css";
+  import Header from "$lib/layout/header.svelte";
 
   let { children } = $props();
 </script>
@@ -15,4 +16,5 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header />
 {@render children()}

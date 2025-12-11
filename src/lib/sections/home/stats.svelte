c160b6893f4ad17f8bb969bbd8cc6caf35d27f7e
Dokja620 <revanamike620@gmail.com>
2025-12-11 16:08:24 +0300

    feat(stats): work like a charm

---
+++ src/lib/sections/home/stats.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { IsInViewport } from "runed";
+
+  const stats = [
+    {
+      numbers: "009",
+      label: "Antennes",
+    },
+    {
+      numbers: "300",
+      label: "Membres",
+    },
+    {
+      numbers: "100",
+      label: "Sites visités",
+    },
+    {
+      numbers: "008",
+      label: "Missions réalisées",
+    },
+  ];
+
+  let targetNode = $state<HTMLElement>()!;
+  const inViewport = new IsInViewport(() => targetNode);
+  $inspect(inViewport);
+</script>
+
+<section id="stats">
+  <div class="content" data-screenshift>
+    {#each stats as stat, i}
+      <div class="stat" data-index={i}>
+        <h1 bind:this={targetNode} class={inViewport ? "is-in-view" : ""}>
+          {#each String(stat.numbers).split("") as char, index}
+            <span data-key={index}>{char}</span>
+          {/each}
+        </h1>
+        <h5>{stat.label}</h5>
+      </div>
+    {/each}
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   import Hero from "$lib/sections/home/hero.svelte";
+  import Stats from "$lib/sections/home/stats.svelte";
 </script>
 
 <Hero />
+
+<Stats />

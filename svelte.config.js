266bc1523e13438f80903ce547494b3abc890729
Dokja620 <revanamike620@gmail.com>
2025-12-15 17:59:14 +0300

    fix(adapter): changed to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,27 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      // See below for an explanation of these options
+      config: undefined,
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined
+      },
+      fallback: 'plaintext',
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+  }
 };
 
 export default config;
